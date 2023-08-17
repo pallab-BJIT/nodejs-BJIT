512c59fd914eabb7f01eec5def2830ea7a53e45c
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-17 16:37:51 +0600

    done

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const areaOfRectangle = rectangleClass.area(3, 4);
 
 const perimeterOfRectangle = rectangleClass.perimeter(3, 4);
 
-console.log(`Area of rectangle ${areaOfRectangle}`);
-console.log(`Perimeter of rectangle ${perimeterOfRectangle}`);
+console.log(`Area of rectangle -> ${areaOfRectangle}`);
+console.log(`Perimeter of rectangle -> ${perimeterOfRectangle}`);
