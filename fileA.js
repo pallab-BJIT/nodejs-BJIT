5867ef18e759323f06584d5aad40d831104f55f9
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-17 10:15:05 +0600

    day 1 class done

---
+++ fileA.js
@@ -0,0 +1,10 @@
+const str = "Hello World......";
+const num = 9;
+const myFunc = () => {
+  console.log("file of file A");
+};
+module.exports = {
+  str,
+  num,
+  myFunc,
+};
---
+++ math/sumObject.js
@@ -0,0 +1,7 @@
+const sumObject = {
+  add: (a, b) => {
+    return a + b;
+  },
+};
+
+module.exports = sumObject;
