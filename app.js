a653298c4268cbd751f6f7dad5327b2f4ecb9f8b
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-17 10:23:08 +0600

    nodeja day 1 task done
    Created a class called Rectangle.Then Inside that class implemented to method one for finding the area of rectangle and second method is for finding the perimeter of rectangle

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-// const { str, num, myFunc } = require("./fileA");
-// // const ClassA = require("./classA");
-// // const classObj = new ClassA("Pallab Majumdar", 20);
-// // classObj.myFunction();
-// // const numImport = require("./fileA");
-// setInterval(() => {
-//   console.log(str);
-//   console.log(num);
-// }, 5000);
-// myFunc();
+const rectangleClass = require("./rectangle");
 
-// const objectA = require("./objectA");
+const areaOfRectangle = rectangleClass.area(3, 4);
 
-// objectA.methodA();
-// objectA.methodB();
+const perimeterOfRectangle = rectangleClass.perimeter(3, 4);
 
-// const Math = require("./math");
-// const add = Math.add(3, 4);
-// console.log(add);
-
-const mathObject = require("./math/sumObject");
-
-const sum = mathObject.add(3, 4);
-
-console.log(`The sum is ${sum}`);
+console.log(`Area of rectangle ${areaOfRectangle}`);
+console.log(`Perimeter of rectangle ${perimeterOfRectangle}`);
