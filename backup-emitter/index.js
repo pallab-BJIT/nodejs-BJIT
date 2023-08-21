55468320417f1479fecf740cd3286413a130e1f7
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-21 14:08:01 +0600

    make the code more simple

--- backup-emitter/index.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-
-class BackupEventEmitter extends EventEmitter {
-    backup() {
-        this.emit('backup');
-    }
-}
-
-module.exports = new BackupEventEmitter();
