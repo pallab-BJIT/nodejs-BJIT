0ae9c96d87301e57189ef4ddeeafc0df62eac8f2
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-22 15:36:45 +0600

    day 4 task done

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const Product = require('./product');
+const http = require('http');
 
-const main = () => {
-    //   const total = Product.getAll();
-    //   //   console.log(total);
-    //! ADD Data
-    // const dataAdded = Product.add({
-    //     name: 'Naruto, Vol. 1',
-    //     price: 9.99,
-    //     stock: 42,
-    //     author: 'Pallab Majumdar',
-    // });
-    // console.log(dataAdded);
-    //! Update Data
-    // const updatedData = Product.updateById(11, {
-    //     publishedAt: '30 Aug 2023',
-    // });
-    // console.log(updatedData);
-    //! Delete Data
-    const deleteById = Product.deleteById(11);
-    console.log(deleteById);
-};
-main();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'Hello, world!', status: 'ok' }));
+        res.end();
+    }
+    if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(
+            JSON.stringify({ message: 'This is the about page', status: 'ok' })
+        );
+        res.end();
+    }
+});
+
+server.listen(8000, () => {
+    console.log('Server listening on 8000');
+});
--- backup.js
+++
@@ -1,8 +0,0 @@
-const { backupMangaJson } = require('./backup-controller');
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-emitter.on('backup', backupMangaJson);
-
-setInterval(() => {
-    emitter.emit('backup');
-}, 2000);
