c911e5e860a2b560a21ddc93f75df3b672d560df
Pallab Majumdar <kitkatpallab@gmail.com>
2023-08-23 21:52:57 +0600

    fixed some logic in query params

--- controllers/data-controller/getAllData.js
+++ controllers/data-controller/getAllData.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const url = require('url');
+const { failure, success } = require('../../util/common');
 
 const getAllData = (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
     try {
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
         const path = './data/manga.json';
         let parsedUrl;
         let queryParams;
@@ -22,20 +23,27 @@ const getAllData = (req, res) => {
                     return ele.name === nameQuery;
                 });
                 if (req.url.includes('?')) {
-                    res.end(JSON.stringify(filteredData));
+                    if (!filteredData.length) {
+                        res.end(JSON.stringify(failure('No data found')));
+                    } else {
+                        res.end(
+                            JSON.stringify(success('Data found', filteredData))
+                        );
+                    }
                 } else {
-                    res.end(JSON.stringify(jsonData));
+                    res.end(JSON.stringify(success('Data found', jsonData)));
                 }
             } else {
                 res.end(
-                    JSON.stringify({
-                        message: 'May be the file is corrupted',
-                    })
+                    JSON.stringify(failure('May be the file is corrupted'))
                 );
             }
         });
     } catch (error) {
-        res.end(JSON.stringify({ message: 'Internal Server Error' }));
+        res.writeHead(500, {
+            'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify(failure('Internal Server Error')));
     }
 };
 
