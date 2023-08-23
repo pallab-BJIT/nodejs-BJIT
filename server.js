cb0aca49e325202f6d8157f89a7782d20a4488b1
Pallab Majumdar <kitkatpallab@gmail.com>
2023-08-23 17:57:59 +0600

    added a route for deleting files

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const addNewData = require('./controllers/data-controller/addData');
 const getAllData = require('./controllers/data-controller/getAllData');
-const { getOneById } = require('./product');
-const fs = require('fs');
+
 const getSingleDataDetails = require('./controllers/data-controller/getSingleData');
 const { failure } = require('./util/common');
+const deleteSingleFileById = require('./controllers/data-controller/deleteSingleFile');
+
 const server = http.createServer((req, res) => {
     if (req.url === '/products/all' && req.method === 'GET') {
         getAllData(req, res);
@@ -15,6 +16,11 @@ const server = http.createServer((req, res) => {
         req.method === 'GET'
     ) {
         getSingleDataDetails(req, res);
+    } else if (
+        req.url.startsWith('/products/deleteOneById/') &&
+        req.method === 'DELETE'
+    ) {
+        deleteSingleFileById(req, res);
     } else {
         res.writeHead(500, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(failure('Wrong Route')));
